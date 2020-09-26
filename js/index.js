47fe33830ffb61359ba9698430bf645647d61e30
Lúcia Vulcano <vulcanolucia@gmail.com>
2020-09-26 16:56:40 -0300

    ...

---
+++ js/index.js
@@ -0,0 +1,7 @@
+let botaoFormularioUm = document.querySelector('#primeiroBotao');
+let inputValueNomeUm = document.querySelector('#inputNomeUm')
+let inputValueNomeDois = document.querySelector('#inputNomeDois')
+
+botaoFormularioUm.addEventListener('click', function () {
+  console.log('oi');
+});
