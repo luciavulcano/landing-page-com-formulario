44b7394004f0d712ae4397f35d20dfe4d242bf60
Lúcia Vulcano <vulcanolucia@gmail.com>
2020-09-28 16:06:18 -0300

    1.8

--- js/index.js
+++ js/index.js
@@ -1,20 +1,21 @@
 let botaoFormularioUm = document.querySelector('#primeiroBotao');
-let inputValueNomeUm = document.querySelector('#inputNomeUm')
-let inputValueEmailUm = document.querySelector('#inputEmailUm')
+let inputValueNomeUm = document.querySelector('#inputNomeUm');
+let inputValueEmailUm = document.querySelector('#inputEmailUm');
 
-let arrayStorage = [];
+let dadosArray = [];
 
-botaoFormularioUm.addEventListener('click', function () {
-  let nome = localStorage.setItem("nome", inputValueNomeUm.value)
-  let email = localStorage.setItem("email", inputValueEmailUm.value)
-  let cliente;
-  if (arrayStorage === null) {
-    arrayStorage === cliente === JSON.stringify({
-      Nome: nome,
-      Email: email
-    });
-    arrayStorage.push(cliente);
+botaoFormularioUm.addEventListener("click", function (e) {
+  e.preventDefault();
+  let nomeUm = inputValueNomeUm.value;
+  let emailUm = inputValueEmailUm.value;
+
+  let cliente = {
+    nome: nomeUm,
+    email: emailUm
   }
+
+  dadosArray.push(cliente);
+  localStorage.setItem("tabelaClientes", dadosArray);
 
 });
 
@@ -22,7 +23,8 @@ let botaoFormularioDois = document.querySelector('#segundoBotao');
 let inputValueNomeDois = document.querySelector('#inputNomeDois');
 let inputValueEmailDois = document.querySelector('#inputEmailDois');
 
-botaoFormularioDois.addEventListener('click', function () {
-  localStorage.setItem("nome", inputValueNomeDois.value)
-  localStorage.setItem("email", inputValueEmailDois.value)
-});
+
+
+
+
+
