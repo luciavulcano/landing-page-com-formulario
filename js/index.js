e43788b6fd25f67d5cabe65b6339d26ca7ab984f
Lúcia Vulcano <vulcanolucia@gmail.com>
2020-09-28 11:29:42 -0300

    1.6

--- js/index.js
+++ js/index.js
@@ -1,7 +1,25 @@
 let botaoFormularioUm = document.querySelector('#primeiroBotao');
 let inputValueNomeUm = document.querySelector('#inputNomeUm')
-let inputValueNomeDois = document.querySelector('#inputNomeDois')
+let inputValueEmailUm = document.querySelector('#inputEmailUm')
+
+let arrayStorage = [];
 
 botaoFormularioUm.addEventListener('click', function () {
-  console.log('oi');
+  let nome = localStorage.setItem("nome", inputValueNomeUm.value)
+  let email = localStorage.setItem("email", inputValueEmailUm.value)
+
+  let cliente = JSON.stringify({
+    Nome: nome,
+    Email: email
+  });
+  arrayStorage.push(cliente);
+});
+
+let botaoFormularioDois = document.querySelector('#segundoBotao');
+let inputValueNomeDois = document.querySelector('#inputNomeDois');
+let inputValueEmailDois = document.querySelector('#inputEmailDois');
+
+botaoFormularioDois.addEventListener('click', function () {
+  localStorage.setItem("nome", inputValueNomeDois.value)
+  localStorage.setItem("email", inputValueEmailDois.value)
 });
