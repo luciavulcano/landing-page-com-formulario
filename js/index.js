361bc155fe64c3ba2f9fbbd59cf82afd15f4ab77
Lúcia Vulcano <vulcanolucia@gmail.com>
2020-09-29 18:11:22 -0300

    2.1

--- js/index.js
+++ js/index.js
@@ -13,17 +13,13 @@ botaoFormularioUm.addEventListener("click", function (e) {
     nome: nomeUm,
     email: emailUm
   }
-
+  validaNome(nomeUm);
+  validaEmail(emailUm);
   dadosArray.push(cliente);
   localStorage.setItem("tabelaClientes", dadosArray);
 
-  function mostraClientes() {
-
-    for (let i = 0; i < dadosArray.length; i++) {
-      console.log(dadosArray[i].nome + " " + dadosArray[i].email);
-    }
-  }
   mostraClientes();
+  botaoFormularioUm.onclick = window.open('obrigado.html', '_blank');
 
 });
 
@@ -40,20 +36,48 @@ botaoFormularioDois.addEventListener("click", function (e) {
     nome: nomeDois,
     email: emailDois
   }
-
+  validaNome(nomeDois);
+  validaEmail(emailDois);
   dadosArray.push(cliente);
   localStorage.setItem("tabelaClientes", dadosArray);
-
-  function mostraClientes() {
-
-    for (let i = 0; i < dadosArray.length; i++) {
-      console.log(dadosArray[i].nome + " " + dadosArray[i].email);
-    }
-  }
   mostraClientes();
+  botaoFormularioDois.onclick = window.open('obrigado.html', '_blank');
 
 });
 
+
+
+function mostraClientes() {
+  for (let i = 0; i < dadosArray.length; i++) {
+    console.log(`nome:${dadosArray[i].nome}, e-mail:${dadosArray[i].email}`);
+  }
+}
+
+let erroEmail = document.querySelector('.mensagem-de-erro__email');
+
+function validaEmail(email) {
+  let regex = /^([A-Za-z0-9_\-\.])+\@([A-Za-z0-9_\-\.])+\.([A-Za-z]{2,4})$/;
+  if (regex.test(email) === false) {
+    return erroEmail.innerHTML = `
+    <p>*Favor inserir um e-mail válido</p>
+    `
+  } else {
+    return erroEmail.innerHTML = `<p></p>`
+  }
+}
+
+let erroNome = document.querySelector('.mensagem-de-erro__nome');
+function validaNome(nome) {
+
+  if (nome.length < 2) {
+    return erroNome.innerHTML = `
+      <p>*Favor inserir um nome válido</p>
+  `
+  }
+  else {
+    return erroNome.innerHTML = `<p></p>`
+  }
+}
 
 
 
