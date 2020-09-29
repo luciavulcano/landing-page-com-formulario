409e5a2c57e466816c3be518d7ed4df28c115fa3
Lúcia Vulcano <vulcanolucia@gmail.com>
2020-09-29 13:57:38 -0300

    2.0

--- js/index.js
+++ js/index.js
@@ -17,14 +17,42 @@ botaoFormularioUm.addEventListener("click", function (e) {
   dadosArray.push(cliente);
   localStorage.setItem("tabelaClientes", dadosArray);
 
-  onclick = "window.location.href='obrigado.html'"
+  function mostraClientes() {
+
+    for (let i = 0; i < dadosArray.length; i++) {
+      console.log(dadosArray[i].nome + " " + dadosArray[i].email);
+    }
+  }
+  mostraClientes();
+
 });
 
 let botaoFormularioDois = document.querySelector('#segundoBotao');
 let inputValueNomeDois = document.querySelector('#inputNomeDois');
 let inputValueEmailDois = document.querySelector('#inputEmailDois');
 
+botaoFormularioDois.addEventListener("click", function (e) {
+  e.preventDefault();
+  let nomeDois = inputValueNomeDois.value;
+  let emailDois = inputValueEmailDois.value;
 
+  let cliente = {
+    nome: nomeDois,
+    email: emailDois
+  }
+
+  dadosArray.push(cliente);
+  localStorage.setItem("tabelaClientes", dadosArray);
+
+  function mostraClientes() {
+
+    for (let i = 0; i < dadosArray.length; i++) {
+      console.log(dadosArray[i].nome + " " + dadosArray[i].email);
+    }
+  }
+  mostraClientes();
+
+});
 
 
 
